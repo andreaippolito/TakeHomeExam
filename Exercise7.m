3fc184e9832cf2b293104105ed5b84b20ddabd3f
RobbertHuijsman <74864210+RobbertHuijsman@users.noreply.github.com>
2020-12-16 13:34:58 +0100

    Add files via upload

--- Exercise7.m
+++ Exercise7.m
@@ -1,20 +1,27 @@
 tolerance=10^-6;
 epsilon=1;
 
-for ii= 4 : 8
-    while epsilon>10^-3
+for ii= 4 : 4
+    %while epsilon>10^-3
+    while epsilon>0.004
     N=2^ii;
     initialGuess=zeros(N-1, 1);
-    [problemMatrix, knownTerm]=ProblemGenerator(N, 0.5);
+    [problemMatrix, knownTerm]=ProblemGenerator(N, epsilon);
     [residualNorms, numberOfIterations]=ForwardGaussSeidelSolver(problemMatrix ,knownTerm, tolerance, initialGuess);
-    red=log(residualNorms/max(abs(knownTerm)));
-    for k=max(2,numberOfIterations-2):numberOfIterations+1
-        residualNorms(k)/residualNorms(k-1);
-    end
+    %red=log(residualNorms/max(abs(knownTerm)));
+    red=residualNorms/max(abs(knownTerm));
+
+    %for k=max(2,numberOfIterations-2):numberOfIterations+1;
+    %    residualNorms(k)/residualNorms(k-1);
+    %end
     epsilon=epsilon/2;
-    plot( linspace(1,numberOfIterations+1,numberOfIterations+1), red);
-    hold on;
-    end
-figure;
+    semilogy ( linspace(1,numberOfIterations+1,numberOfIterations+1), red, 'DisplayName', string(epsilon));
+    lgd = legend;
+    hold on
+    end    
+
+lgd.NumColumns = 2;
+
 epsilon=1;
+figure;
 end
